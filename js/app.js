a458aaec9430a791170f1b9409868ac822c823e2
Dimma <josephemeka2611@gmail.com>
2021-05-10 16:01:00 +0100

    implement page routing

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import { HomeScreen } from "./HomeScreen.js";
-import ProductScreen from "./ProductScreen.js";
-
-const route = {
-  "/": HomeScreen,
-  "Product /:id": ProductScreen,
-};
-
-HomeScreen.render();
